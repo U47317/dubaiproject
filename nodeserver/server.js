ea30f15233da17d8f8ecfe8cbe95fc93c2ae0008
Arun Mohan <arunmohan@Aruns-MacBook-Pro.local>
2019-08-31 22:42:50 +0530

    Updated nodeserver

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -3,4 +3,10 @@ const app = express()
 const port = 3001
 app.listen(port, () => console.log(`Listnening to port ${port}!`))
 
-
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/kiosk',{useNewUrlParser: true})
+var db = mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error!'));
+db.once('open',function callback(){
+    console.log('Connected to Mongo Database!!')
+})
